4cda47c0ac8cecd8e4371f18142feb1372b55e02
Patrick Miller <pjmiller823@gmail.com>
2017-05-17 12:48:29 -0400

    added user controller with show. Imported profile mockup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'session#new'
+
+  # session routes
   get    '/auth/:provider'          => 'omniauth#auth',    as: :auth
   get    '/auth/:provider/callback' => 'session#create'
   get    '/auth/failure'            => 'session#failure'
 
-  root 'session#new'
   get '/login' => 'session#new'
   post '/login' => 'session#create'
   get '/logout' => 'session#destroy'
+
+  # user routes
+  get '/profile' => 'user#show'
 end
