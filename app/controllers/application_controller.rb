c5dbb02e97089f124ee9275a151b2783031aac93
Patrick Miller <pjmiller823@gmail.com>
2017-05-17 08:29:56 -0400

    added user model and set up sessions. Need login button.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user=(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+  helper_method :logged_in?
+
+  # Method to use in filter to ensure the user is logged in
+  def authenticate!
+    unless logged_in?
+      redirect_to auth_path(provider: 'github')
+    end
+  end
 end
