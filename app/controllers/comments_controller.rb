d4f86695d96285f9fd0946fbadd9b90d433ae402
Patrick Miller <pjmiller823@gmail.com>
2017-05-21 15:04:53 -0400

    Comments now track name of user who wrote them.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -27,7 +27,7 @@ class CommentsController < ApplicationController
   def create
     @submission = Submission.find(params[:submission_id])
     @comment = @submission.comments.create(comment_params)
-    @comment.created_by_id = current_user.id
+    @comment.created_by = current_user
 
     if @comment.save
       redirect_to membership_submission_path(@submission.membership.id, @submission.id), notice: 'Comment was successfully created.'
