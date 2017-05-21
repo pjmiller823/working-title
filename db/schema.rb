2277b0eaca435ec31893ccf391ba3c30cf98c935
Patrick Miller <pjmiller823@gmail.com>
2017-05-21 16:51:32 -0400

    generated invites model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170521151945) do
+ActiveRecord::Schema.define(version: 20170521205016) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20170521151945) do
     t.datetime "updated_at", null: false
     t.integer  "user_id"
     t.string   "name"
+  end
+
+  create_table "invites", force: :cascade do |t|
+    t.string   "email"
+    t.integer  "group_id"
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.string   "token"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
   create_table "memberships", force: :cascade do |t|
