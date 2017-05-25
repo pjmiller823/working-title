e485800f80f397ece06885671349ae21a865ea08
Patrick Miller <pjmiller823@gmail.com>
2017-05-24 20:46:46 -0400

    styling fixes on group and author show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170523162049) do
+ActiveRecord::Schema.define(version: 20170524225108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20170523162049) do
     t.datetime "updated_at",    null: false
     t.integer  "membership_id"
     t.date     "due_by"
+    t.string   "title"
   end
 
   create_table "users", force: :cascade do |t|
